88e721287d113b4b9fa0735e64ec5b08e2dcc399
realtroffy <artur.asiptsov@yandex.ru>
2022-12-18 20:13:33 +0300

    Sheduling to CHGK question

--- src/main/java/telegrambot/sсheduled/Scheduler.java
+++ src/main/java/telegrambot/sсheduled/Scheduler.java
@@ -1,1 +1,26 @@
+package telegrambot.sсheduled;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import telegrambot.parser.xmlimpl.CHGKXmlParser;
+
+import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.Queue;
+
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+
+  public static final Queue<Map<String, Object>> chgkQueue = new ArrayDeque<>();
+  private final CHGKXmlParser parser;
+
+  @Scheduled(cron = "@daily")
+  public void addCHGKQuestionToCashQueue() throws JsonProcessingException {
+    if (chgkQueue.size() < 30) {
+      chgkQueue.add(parser.processQuestionButton());
+    }
+  }
+}
